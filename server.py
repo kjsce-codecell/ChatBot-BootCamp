23a49148369b4fb91c059776a41c311534e979d1
nurdtechie98 <nurdtechie98@gmail.com>
2019-03-22 02:55:54 +0530

    pass parameters to event

--- server.py
+++ server.py
@@ -4,12 +4,11 @@ import requests
 
 app = Flask(__name__)
 url = 'https://api.funtranslations.com/translate/'
-final_url = ''
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
   req = request.get_json(silent=True, force=True)
-  print(json.dumps(req, indent=4, sort_keys=True))
-  print(req['queryResult'])
+  print(req)
   try:
     intent = req.get('queryResult').get('intent').get('displayName')
   except AttributeError:
@@ -17,18 +16,22 @@ def webhook():
   print('Intent: ' + intent)
   if intent == 'Translation Init':
     fulfillment = req.get('queryResult').get('fulfillmentText')
-    global final_url
-    final_url = url +req.get('queryResult').get('parameters').get('Language')[0]+'.json'
-    print(final_url) 
-    return make_response(jsonify({'fulfillmentText': fulfillment,'followupEventInput': {'name': 'Translate','languageCode': 'en-US'}}))
+    language = req.get('queryResult').get('parameters').get('Language')[0]
+    return make_response(jsonify({'fulfillmentText': fulfillment,'followupEventInput': {'name': 'Translate','parameters':{'language':language},'languageCode': 'en-US'}}))
   elif intent == 'Translate':
     query = req.get('queryResult').get('queryText')
-    print(query,'*****',final_url)
+    language = req.get('queryResult').get('parameters').get('language')
     payload = {'text':query}
+    final_url = url + language + '.json'
     res = requests.post(final_url,params=payload)
-    text = res.json()["contents"]["translated"]
+    if res.status_code == 200:
+      text = res.json()["contents"]["translated"]
+    else:
+      print(res.status_code)
+      text = "request overlimit"
     return make_response(jsonify({'fulfillmentText': text}))
   else:
     return make_response(jsonify({'fulfillmentText': 'Apna Time Aega'}))
+
 if __name__ == '__main__':
   app.run()
