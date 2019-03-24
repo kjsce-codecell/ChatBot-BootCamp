64b6d77529524e3d6ca841fdbbe5c98fa995f5f9
nurdtechie98 <nurdtechie98@gmail.com>
2019-03-24 13:52:29 +0530

    add numbers api

--- server.py
+++ server.py
@@ -1,37 +1,42 @@
 from flask import Flask, request, make_response, jsonify
 import json
 import requests
+import random
 
 app = Flask(__name__)
-url = 'https://api.funtranslations.com/translate/'
+url = 'http://numbersapi.com/'
+
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-  req = request.get_json(silent=True, force=True)
-  print(req)
-  try:
+    req = request.get_json(silent=True, force=True)
+    print(req)
     intent = req.get('queryResult').get('intent').get('displayName')
-  except AttributeError:
-    return 'json error'
-  print('Intent: ' + intent)
-  if intent == 'Translation Init':
-    fulfillment = req.get('queryResult').get('fulfillmentText')
-    language = req.get('queryResult').get('parameters').get('Language')[0]
-    return make_response(jsonify({'fulfillmentText': fulfillment,'followupEventInput': {'name': 'Translate','parameters':{'language':language},'languageCode': 'en-US'}}))
-  elif intent == 'Translate':
-    query = req.get('queryResult').get('queryText')
-    language = req.get('queryResult').get('parameters').get('language')
-    payload = {'text':query}
-    final_url = url + language + '.json'
-    res = requests.post(final_url,params=payload)
-    if res.status_code == 200:
-      text = res.json()["contents"]["translated"]
-    else:
-      print(res.status_code)
-      text = "request overlimit"
-    return make_response(jsonify({'fulfillmentText': text}))
-  else:
+    print(intent)
+    if intent == 'Default Welcome Intent':
+        return make_response(jsonify({'fulfillmentText': 'Welcome detetcted'}))
+
+    elif intent == 'numbers':
+        # Extarct the text
+        qtype = req.get('queryResult').get('parameters').get('type')
+        num = req.get('queryResult').get('parameters').get('number')
+        if qtype == 'random':
+            q = ['trivia', 'math', 'year']
+            qtype = random.choice(q)
+
+        # Send a API request
+        final_url = url + str(int(num)) + '/' + qtype + '?json'
+        print(final_url)
+        res = requests.get(final_url)
+        # Extract response
+        print(res.json())
+        text = res.json()["text"]
+
+        # Return
+        return make_response(jsonify({'fulfillmentText': text}))
+
     return make_response(jsonify({'fulfillmentText': 'Apna Time Aega'}))
 
+
 if __name__ == '__main__':
-  app.run()
+    app.run()
